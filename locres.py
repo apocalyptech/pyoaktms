f7843f438fbaa7bdfe8de1f74a59574c7dbbecb9
CJ Kucera <cj@apocalyptech.com>
2022-04-01 01:12:19 -0500

    Fixing up wording in our GPL headers

--- locres.py
+++ locres.py
@@ -5,12 +5,12 @@
 # <cj@apocalyptech.com>
 # <http://apocalyptech.com/contact.php>
 #
-# PyOakTMS is free software: you can redistribute it
+# PyOakTMS/locres.py is free software: you can redistribute it
 # and/or modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 #
-# Borderlands 3 Hotfix Modding Library is distributed in the hope that it will
+# PyOakTMS/locres.py is distributed in the hope that it will
 # be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
--- oaktms.py
+++ oaktms.py
@@ -10,7 +10,7 @@
 # as published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 #
-# Borderlands 3 Hotfix Modding Library is distributed in the hope that it will
+# PyOakTMS is distributed in the hope that it will
 # be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
